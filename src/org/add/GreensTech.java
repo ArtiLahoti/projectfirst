e3d8dadb3fd995bf06aa256cfe535cf2b24115cc
ArtiLahoti <monamantri1@gmail.com>
2021-02-12 11:18:05 -0500

    completed

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,32 @@
+package org.add;
+
+public class GreensTech {// greensOmr(),greensAdayar(),greensTambaram(),greensVelacherry(),greensAnnaNagar()
+	public void greensOmr() {
+		System.out.println("amit");
+	}
+
+	public void greensAdayar() {
+		System.out.println("grishma");
+	}
+
+	public void greensTambaram() {
+		System.out.println("vaibhav");
+	}
+
+	public void greenaVelacherry() {
+		System.out.println("sir");
+	}
+
+	public void greensAnnanagar() {
+		System.out.println("kiran");
+	}
+
+	public static void main(String[] args) {
+		GreensTech gt = new GreensTech();
+		gt.greensOmr();
+		gt.greensAdayar();
+		gt.greensTambaram();
+		gt.greenaVelacherry();
+		gt.greensAnnanagar();
+	}
+}
---
+++ src/org/add/Stringclasses.java
@@ -0,0 +1,32 @@
+package org.add;
+
+public class Stringclasses {
+public static void main(String[] args) {
+	//String a= new String (" helloworld");
+	//String b=new String("magicalworld");
+	//System.out.println(a.charAt(2));
+	//System.out.println(a.indexOf("e"));
+	//a.substring(2, 6);
+	//a.substring(5);
+	//System.out.println(a.concat(b));
+	//System.out.println(a.length());
+	//System.out.println(a.trim());
+	//System.out.println(a.toUpperCase());
+	//System.out.println(a.toLowerCase());
+	//String arr[]=a.split("w");
+	//System.out.println(arr[0]);
+	//System.out.println(arr[1]);
+	//System.out.println(a.replace("e", "a"));
+	String s= "arti";
+	String t="";
+	for (int i=s.length()-1; i>=0; i--)
+	{  t=t+s.charAt(i);
+	
+	
+	}System.out.println(t);
+	}
+	
+	
+	
+}
+
